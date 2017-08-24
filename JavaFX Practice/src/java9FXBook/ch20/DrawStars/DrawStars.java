c350e41c624b9e4fcac53aaf2a0d752a01b30ee0
Chinedu <eduboy@yahoo.com>
2017-08-24 14:08:08 +0100

    Ok java book essential
    Chapter 12,13 20

---
+++ JavaFX Practice/src/java9FXBook/ch20/DrawStars/DrawStars.java
@@ -0,0 +1,24 @@
+package java9FXBook.ch20.DrawStars;
+// DrawStars.java
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class DrawStars extends Application {
+   @Override
+   public void start(Stage stage) throws Exception {
+      Parent root = FXMLLoader.load(getClass().getResource("DrawStars.fxml"));
+      
+      Scene scene = new Scene(root);
+      stage.setTitle("Draw Stars");
+      stage.setScene(scene);
+      stage.show();
+   }
+
+   public static void main(String[] args) {
+      launch(args);
+   }
+   
+}
